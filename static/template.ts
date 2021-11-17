f3eb4a8cb2c1f620a020dabab025778814879238
kixixixixi <kixixixixi@users.noreply.github.com>
2021-11-17 16:07:53 +0000

    deploy: 415812b383a8e764b8c4669fbb2de600bd218998

---
+++ static/template.ts
@@ -0,0 +1,112 @@
+const menu = {
+  sections: [
+    {
+      section: { title: "カフェ" },
+      items: [
+        {
+          title: "アイスコーヒー",
+          unit: "杯",
+          description: null,
+          quantity: "300ml",
+          cost: 500,
+        },
+        {
+          title: "ホットコーヒー",
+          unit: "杯",
+          description: null,
+          quantity: "200ml",
+          cost: 500,
+        },
+        {
+          title: "アイスティー",
+          unit: "杯",
+          description: null,
+          quantity: "300ml",
+          cost: 500,
+        },
+        {
+          title: "ホットティー",
+          unit: "杯",
+          description: null,
+          quantity: "200ml",
+          cost: 500,
+        },
+        {
+          title: "ルイボスティー",
+          unit: "杯",
+          description: null,
+          quantity: "300ml",
+          cost: 500,
+        },
+        {
+          title: "ジャスミンティー",
+          unit: "杯",
+          description: null,
+          quantity: "300ml",
+          cost: 500,
+        },
+      ],
+    },
+    {
+      section: { title: "アルコール" },
+      items: [
+        {
+          title: "ビール",
+          unit: "杯",
+          description: "サッポロ",
+          quantity: "334ml",
+          cost: 500,
+        },
+        {
+          title: "クラフトビール",
+          unit: "杯",
+          description: "別紙参照",
+          quantity: "300ml",
+          cost: 700,
+        },
+        {
+          title: "ハイボール",
+          unit: "杯",
+          description: "ソーダ or ジンジャエール",
+          quantity: "300ml",
+          cost: 500,
+        },
+        {
+          title: "酎ハイ",
+          unit: "杯",
+          description: null,
+          quantity: "300ml",
+          cost: 500,
+        },
+        {
+          title: "レモンサワー",
+          unit: "杯",
+          description: null,
+          quantity: "300ml",
+          cost: 500,
+        },
+      ],
+    },
+    {
+      section: { title: "スナック" },
+      items: [
+        {
+          title: "ミックスナッツ",
+          unit: "皿",
+          description: null,
+          quantity: "1",
+          cost: 300,
+        },
+        {
+          title: "サラミ",
+          unit: "皿",
+          description: null,
+          quantity: "1",
+          cost: 300,
+        },
+      ],
+    },
+  ],
+}
+
+export default menu
