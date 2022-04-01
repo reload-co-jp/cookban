a838aed119be61b9f74fe086c6e3117e120f00b5
kixixixixi <kixixixixi@users.noreply.github.com>
2022-04-01 18:59:03 +0000

    deploy: aba690454662656189b22548e31c149f64821f48

---
+++ _next/static/HuctXmJNXP1kdcOS-2FJC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/934-58798ee9b3b69297d506.js","static/chunks/pages/index-d136865f327f407182e5.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
