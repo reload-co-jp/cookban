813441d63b3c855b579e8384473b4109e7a96b7a
kixixixixi <kixixixixi@gmail.com>
2022-04-02 03:46:25 +0900

    Remove point

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,6 @@ import {
   Section,
   ItemCard,
 } from "components/elements"
-import Styled, { keyframes } from "styled-components"
 import {
   GoogleSpreadSheetAPIResponse,
   Item,
@@ -26,53 +25,10 @@ const spreadSheetHeader = [
   "active",
 ] as const
 
-const PointAnimation = keyframes`
-  100% {
-    opacity: 0;
-    top: 50%;
-  }
-`
-const Point = Styled.div`
-  color: #C44303;
-  content: "●";
-  font-size: 36px;
-  left: -36px;
-  position: fixed;
-  top: 200%;
-  text-shadow:
-  5vw   -10vh 28px,
-  10vw  -40vh 26px,
-  20vw  -50vh 24px,
-  30vw  -58vh 29px,
-  39vw  -25vh 20px,
-  42vw  -34vh 25px,
-  48vw  -80vh 28px,
-  56vw  -15vh 28px,
-  63vw  -18vh 26px,
-  78vw  -22vh 24px,
-  86vw  -32vh 29px,
-  94vw  -17vh 27px;
-  animation: ${PointAnimation} 8s linear infinite;
-  &:nth-child(2) {
-    animation-delay: 2s;
-  }
-  &:nth-child(3) {
-    animation-delay: 4s;
-  }
-  &:nth-child(4) {
-    animation-delay: 6s;
-  }
-`
-
 const IndexPage = ({ items }: { items: SectionItem[] }): React.ReactElement => {
   return (
     <>
       <Layout>
-        {Array(4)
-          .fill(undefined)
-          .map((_, i) => (
-            <Point key={i}>●</Point>
-          ))}
         <Main className="is-dark">
           <Container>
             <Content className="is-large">
